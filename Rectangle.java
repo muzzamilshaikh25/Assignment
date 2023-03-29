c87fc0a8d4276836bad5e4556d6e354a6f672c6f
muzzamilshaikh25 <127470777+muzzamilshaikh25@users.noreply.github.com>
2023-03-29 14:10:37 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,20 @@
+package Com.cognizant.shapes;
+
+public class Rectangle {
+	int l=5;
+	int w=6;
+	//System.out.println(length+" "+width);
+	
+	static int CalculateArea(int l,int w)
+	{
+		return l*w;
+		
+	}
+		
+
+public static void main(String[] args) {
+	System.out.println("The area of Reactangle is calculate by using formula length*breath");
+
+	Rectangle obj=new Rectangle();
+} 
+}
